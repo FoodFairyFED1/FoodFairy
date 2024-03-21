e820d92344db44afbe3f98f6c1e5d37e1b397192
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-03-21 12:45:59 +0100

    chore: API fetch request using ID from URL param

--- src/js/recipe-details.js
+++ src/js/recipe-details.js
@@ -1,13 +1,24 @@
-import {toggleBurgerMenu} from "./header-and-footer.js";
-import {toggleFavsMenu} from "./header-and-footer.js";
+import { toggleBurgerMenu } from "./header-and-footer.js";
+import { toggleFavsMenu } from "./header-and-footer.js";
 
-const toggleBurgerMenuBTN = document.querySelector(".btn-toggle-burger-menu")
-const toggleFavsMenuBTN = document.querySelector(".btn-toggle-fav-menu")
+const toggleBurgerMenuBTN = document.querySelector(".btn-toggle-burger-menu");
+const toggleFavsMenuBTN = document.querySelector(".btn-toggle-fav-menu");
 
-toggleBurgerMenuBTN.addEventListener("click", function(){
-    toggleBurgerMenu()
-})
+toggleBurgerMenuBTN.addEventListener("click", function () {
+  toggleBurgerMenu();
+});
 
-toggleFavsMenuBTN.addEventListener("click", function (){
-    toggleFavsMenu()
-})
+toggleFavsMenuBTN.addEventListener("click", function () {
+  toggleFavsMenu();
+});
+
+async function getRecipe(id) {
+  try {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`
+    );
+    const data = await response.json();
+  } catch (error) {
+    console.log("an error has happened: " + error);
+  }
+}
