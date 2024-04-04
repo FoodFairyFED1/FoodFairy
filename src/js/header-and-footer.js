af8f62cc869799a9e909783a0c1f452adbecf43d
Thomas Eikhaugen <thomas.eikhaugen@gmail.com>
2024-04-04 13:04:11 +0200

    fix: renamed classes in innerhtml on favRecipeCard to be more specific

--- src/js/header-and-footer.js
+++ src/js/header-and-footer.js
@@ -2,6 +2,7 @@ import {API_KEY} from "../../API_KEY.js";
 
 const burger_menu = document.querySelector(".burger-menu");
 const favs_menu = document.querySelector(".favs-menu");
+const favs_container = document.querySelector(".favs-container");
 
 export function toggleBurgerMenu() {
     if (burger_menu.style.display === "none" || burger_menu.style.display === "") {
@@ -37,27 +38,27 @@ export function saveFavourites() {
 export async function getFavourites() {
     const favRecipesString = JSON.parse(window.localStorage.getItem("FoodFairyFavs")) || [];
     const favRecipes = favRecipesString.join();
-        try {
-            const response = await fetch(
-                `https://api.spoonacular.com/recipes/informationBulk?ids=${favRecipes}&apiKey=${API_KEY}`
-            );
-            const recipeData = await response.json();
-            displayFavourites(recipeData);
-        } catch (error) {
-            console.error("Error fetching data:", error);
-        }
+    try {
+        const response = await fetch(
+            `https://api.spoonacular.com/recipes/informationBulk?ids=${favRecipes}&apiKey=${API_KEY}`
+        );
+        const recipeData = await response.json();
+        displayFavourites(recipeData);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
 }
 
 function displayFavourites(recipes) {
-    favs_menu.innerHTML = "";
+    favs_container.innerHTML = "";
     recipes.forEach((recipe) => {
-        favs_menu.innerHTML += `
-        <a class="recipe-card" href="pages/recipe-details.html?id=${recipe.id}">
-            <div class="recipeCardStart">
-                <img class="recipeCardImage" src="${recipe.image}">
+        favs_container.innerHTML += `
+        <a class="favRecipeCard" href="pages/recipe-details.html?id=${recipe.id}">
+            <div class="favRecipeCardStart">
+                <img class="favRecipeCardImage" src="${recipe.image}">
             </div>
-            <div class="recipeCardEnd">
-                <h3 class="recipeCardTitle">${recipe.title}</h3>
+            <div class="favRecipeCardEnd">
+                <h3 class="favRecipeCardTitle">${recipe.title}</h3>
             </div>
         </a>
         `;
