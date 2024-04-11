d14e3833aeff47f1482e8a63d811af93455da79a
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-04-11 20:11:45 +0200

    feat: added see more functionality for consumer

--- src/js/recipe-details.js
+++ src/js/recipe-details.js
@@ -1,46 +1,65 @@
-import {API_KEY} from "../../API_KEY.js";
-import {toggleBurgerMenu} from "./header-and-footer.js";
-import {toggleFavsMenu} from "./header-and-footer.js";
-import {saveFavourites} from "./header-and-footer.js";
+import { API_KEY } from "../../API_KEY.js";
+import { toggleBurgerMenu } from "./header-and-footer.js";
+import { toggleFavsMenu } from "./header-and-footer.js";
+import { saveFavourites } from "./header-and-footer.js";
 
 const toggleBurgerMenuBTN = document.querySelector(".btn-toggle-burger-menu");
 const toggleFavsMenuBTN = document.querySelector(".btn-toggle-fav-menu");
 const recipeDetailInfo = document.querySelector(".recipe-detail-info-section");
-const recipeDetailIngredients = document.querySelector(".recipe-detail-ingredients-section");
-const recipeDetailInstruction = document.querySelector(".recipe-detail-instruction-section");
+const recipeDetailIngredients = document.querySelector(
+  ".recipe-detail-ingredients-section"
+);
+const recipeDetailInstruction = document.querySelector(
+  ".recipe-detail-instruction-section"
+);
 const favouriteBTN = document.querySelector(".favourite-recipe-btn");
 const loader = document.querySelector(".loader");
+const reSummarySection = document.querySelector(".recipe-summary-section");
+const recipeSummary = document.querySelector(".recipe-summary");
+const recipeSummaryBtn = document.querySelector(".summary-more-btn");
 
 toggleBurgerMenuBTN.addEventListener("click", function () {
-    toggleBurgerMenu();
+  toggleBurgerMenu();
 });
 
 toggleFavsMenuBTN.addEventListener("click", function () {
-    toggleFavsMenu();
+  toggleFavsMenu();
 });
 
+let recipeData;
+
 async function getRecipe() {
-    loader.style.display = "flex";
-    const parameterString = window.location.search;
-    const searchParameter = new URLSearchParams(parameterString);
-    const recipeId = searchParameter.get("id");
-    try {
-        const response = await fetch(
-            `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${API_KEY}`
-        );
-        const data = await response.json();
-        console.log(data);
-        displayRecipe(data);
-    } catch (error) {
-        console.log("Error: " + error);
-    }
-    loader.style.display = "none";
-    return recipeId;
+  loader.style.display = "flex";
+  const parameterString = window.location.search;
+  const searchParameter = new URLSearchParams(parameterString);
+  const recipeId = searchParameter.get("id");
+  try {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${API_KEY}`
+    );
+    recipeData = await response.json();
+
+    displayRecipe(recipeData);
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+  loader.style.display = "none";
+  return recipeId;
 }
 
 getRecipe();
 
+function shortenText(text) {
+  const words = text.split(" ");
+  const wordsShort = words.slice(0, 20).join(" ") + "...";
+  return wordsShort;
+}
+
+let recipeSummaryLong;
+
 function displayRecipe(recipe) {
+  const recipeSummaryShort = shortenText(recipe.summary);
+
   recipeDetailInfo.innerHTML += `
 <img src="${recipe.image}" alt="food">
 <div class="img-fav-div">
@@ -63,32 +82,37 @@ function displayRecipe(recipe) {
           ${recipe.dishTypes.join(", ")} 
       </p>
     </div>
-
-    <p>${recipe.summary}</p>
 </div>
 `;
 
-    //I have no idea why this works, but it does, so dont question it
-    let ingredientAmount = [];
-    for (let i = 0; i < recipe.extendedIngredients.length; i++) {
-        ingredientAmount.push(
-            `<div class="recipe-detail-ingredients">
+  recipeSummaryLong = `${recipe.summary}`;
+
+  recipeSummary.innerHTML = `${recipeSummaryShort}`;
+
+  //I have no idea why this works, but it does, so dont question it
+  let ingredientAmount = [];
+  for (let i = 0; i < recipe.extendedIngredients.length; i++) {
+    ingredientAmount.push(
+      `<div class="recipe-detail-ingredients">
       <p class="ingredient-amount-measurements">${recipe.extendedIngredients[i].measures.metric.amount} ${recipe.extendedIngredients[i].measures.metric.unitLong}</p>
       <p class="ingredient-amount-ingredient">${recipe.extendedIngredients[i].name}</p>
       </div>`
-        );
-    }
+    );
+  }
 
-    recipeDetailIngredients.innerHTML += `
+  recipeDetailIngredients.innerHTML += `
   <div >
   <div class="ingredient-amounts">${ingredientAmount.join(" ")}</div>
   </div>
   `;
 
   recipeDetailInstruction.innerHTML += `<div class="recipe-instuctions"><p>${recipe.instructions}</p></div>`;
-
 }
 
-favouriteBTN.addEventListener("click", function(){
-  saveFavourites()
-})
+recipeSummaryBtn.addEventListener("click", function () {
+  recipeSummary.innerHTML = `${recipeSummaryLong}`;
+});
+
+favouriteBTN.addEventListener("click", function () {
+  saveFavourites();
+});
